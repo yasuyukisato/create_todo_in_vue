072da09a3adbf2f239ac8dc6eb99d3f41d288005
yasuyuki sato <godasper@gmail.com>
2021-12-27 00:35:08 +0900

    タスクの追加

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 const app = Vue.createApp({
-  data: () => ({}),
+  data: () => ({
+    newItem: "",
+    todos: [],
+  }),
+  methods: {
+    addItem: function () {
+      console.log("Clicked!");
+      let todo = {
+        item: this.newItem,
+      };
+      this.todos.push(todo);
+    },
+  },
 });
 app.mount("#app");
